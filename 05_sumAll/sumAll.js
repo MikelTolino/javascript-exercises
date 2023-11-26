0f66d9f36fdf6193ee024907702d2eb912919614
mmateo-t <mmateo-t@student.42madrid.com>
2023-11-26 14:25:51 +0100

    ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+function isNumber(value) {
+	return typeof value === 'number';
+}
 
+const sumAll = function (n1, n2) {
+	let sum = 0;
+
+	if (n1 < 0 || n2 < 0)
+		return "ERROR";
+	if (typeof n1 !== "number" || typeof n2 !== "number")
+		return "ERROR"
+
+	if (n1 > n2) {
+		let aux = n1;
+		n1 = n2;
+		n2 = aux;
+	}
+	for (let index = n1; index <= n2; index++) {
+		sum += index;
+	}
+	return sum;
 };
 
 // Do not edit below this line
